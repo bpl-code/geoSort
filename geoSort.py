6a91e70b85221f586db5d7f1e6b7b877665a416c
bpl-code <rplewiscopy@gmail.com>
2019-03-10 13:40:54 +0000

    Began running some tests and trying out features of the GPSPhoto and Geopy modules. I have also began sketching out the Photo class. I have also sketched out the program in comments

--- geoSort.py
+++ geoSort.py
@@ -3,18 +3,47 @@
 
 from GPSPhoto import gpsphoto
 import os
+from geopy.geocoders import Nominatim
 
-fileName = 'IMG_6952.jpg'
-print(gpsphoto.getGPSData(fileName))
 
-os.mkdir('news folder') #creates new folder
+class photo():
 
+    def __init__ (self, location, Latitude, Longitude):
+        self.Latitude = Latitude
+        self.Longitude = Longitude
+        self.city = self.getCity(location)
+        self.address = self.getAddress(location)
+
+    def getCity(self):
+        return 0
+
+
+
+
+data = gpsphoto.getGPSData('IMG_6952.jpg')
+loc = str(data["Latitude"]) + "," + str(data['Longitude'])
+print(loc)
+
+
+
+geolocator = Nominatim(user_agent="geoSort")
+location = geolocator.reverse(loc, language='en-GB')
+print(location.address)
 
 #Choose file directory 
 #Choose naming convension (LATER FEATURE FOR DONT ASK)
+#iterate through all files
+#extract data from file
+#create photo object and save geo and date
+
+#find address and add to photo
+
+#create a sort of locations 
+
 
 def chooseDirectory():
     directory = input("Enter Directory: ")
     return 0
 
-def 
+
+#os.mkdir('a folder') #creates new folder
