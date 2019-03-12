fedd6d08e024d2dca32344d71f7d274ce5d0f130
bpl-code <rplewiscopy@gmail.com>
2019-03-12 09:22:26 +0000

    Fixed the issue with calling location.raw. I forgot to put self before the varible. I have now added a self.fullURL for the file url and a getFullURL function. I have also sketched out the rest of the programs function names and functionality in comments. I need to figure out the UI set up and whether I want it to be ourely terminal based or GUI.

--- geoSort.py
+++ geoSort.py
@@ -8,19 +8,21 @@ from geopy.geocoders import Nominatim
 
 class photo():
 
-    def __init__ (self, photoFile):
+    def __init__ (self, photoFile, fileURL):
+
         self.photoFile = photoFile
+        self.fileURL = fileURL
         self.latitude, self.longitude, self.coordinates = self.findLatitudeAndLongitude()
+        self.geolocator = Nominatim(user_agent="geoSort")
+        self.fullLocationDetials = self.geolocator.reverse(self.getCoordinates(), addressdetails=True, language='en-GB')
+        self.rawAddress = self.fullLocationDetials.raw
+
         self.fullAddress = self.findFullAddress()
         self.city = self.findCity()
         self.country = self.findCountry()
-        self.geolocator = Nominatim(user_agent="geoSort")
-        self.fullLocationDetials = geolocator.reverse(loc, addressdetails=True, language='en-GB')
-        self.rawAddress = fullLocationDetials.raw
-
 
     def findLatitudeAndLongitude(self):
-        gpsData = gpsphoto.getGPSData('IMG_5386.jpg') #needs to be replaced with file
+        gpsData = gpsphoto.getGPSData('IMG_5386.jpg') #needs to be replaced with photoFile
         latitude = str(gpsData["Latitude"])
         longitude = str(gpsData['Longitude'])
         coordinates = latitude + "," + longitude
@@ -38,6 +40,12 @@ class photo():
     def findCountry(self):
         country = self.rawAddress['address']['city']
         return country
+
+    def getFileURL(self):
+        return self.fileURL
+
+    def getCoordinates(self):
+        return self.coordinates
 
     def getLatitude(self):
         return self.latitude
@@ -71,11 +79,57 @@ def chooseDirectory():
     directory = input("Enter Directory: ")
     return 0
 
+def loadConfigFile():
+    #look for a config file
+    #update locations sort list with list from config file
+    #if no config file return False and run createConfigFile()
+    
+    return 0
+
+def saveConfigFile():
+    #update config file
+    return 0
+
+def createConfigFile():
+    return 0
+
+def processPhotoFiles():
+    #for:
+    #selectFiles()
+    #run through all jpegs automatically and create a new photofile for each 
+    #createPhotoFile()
+    #save them to a list return list
+    return 0
+
+def addNewLocations(photoList):
+    #run through photos adding any new locations to sort list
+    return 
+
+def organiseFiles(photoList, locationList):
+    #sort which folder the files need to move to and use moveFile()
+    return 0 
+
+def selectFile():
+    #select next file and return it
+    return 0
+
+def createPhotoFile(file):
+    newPhotoFile = 'file'
+    return 0
+
+def createFolders():
+    #create new folder from sort list 
+    #save new folder to config file
+    return 0 
+
+def moveFiles(file, targetLocation):
+    #move file to new location
+    return
 
 #os.mkdir('a folder') #creates new folder
 
 def main():
-    pic = photo('file')
+    pic = photo('file', 'File')
     print(pic.getCity())
 
 if __name__ == "__main__":
